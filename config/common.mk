9bbb279e45c5e58d8a685fcdbecdcc6bfeecc77f
Robbie <RobbieLancaster811@gmail.com>
2015-04-03 01:19:52 -0600

    Set my build version
    Signed-off-by: Robbie <RobbieLancaster811@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -258,7 +258,7 @@ ifdef CM_BUILDTYPE
     endif
 else
     # If CM_BUILDTYPE is not defined, set to UNOFFICIAL
-    CM_BUILDTYPE := UNOFFICIAL
+    CM_BUILDTYPE := ROBBIEL811-UBER4.8
     CM_EXTRAVERSION :=
 endif
 
