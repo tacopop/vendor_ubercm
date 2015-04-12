bddfdfb47a751766b39551e76d7a173b6f41e9e7
Robbie <RobbieLancaster811@gmail.com>
2015-04-12 04:16:05 -0600

    Get rid of a bunch of unused wallpapers
    Signed-off-by: Robbie <RobbieLancaster811@gmail.com>

--- config/common_full.mk
+++ config/common_full.mk
@@ -9,17 +9,8 @@ PRODUCT_PACKAGE_OVERLAYS += vendor/cm/overlay/dictionaries
 
 # Optional CM packages
 PRODUCT_PACKAGES += \
-    Galaxy4 \
-    HoloSpiralWallpaper \
-    LiveWallpapers \
-    LiveWallpapersPicker \
-    MagicSmokeWallpapers \
-    NoiseField \
-    PhaseBeam \
-    VisualizationWallpapers \
     PhotoTable \
-    SoundRecorder \
-    PhotoPhase
+    SoundRecorder
 
 # Extra tools in CM
 PRODUCT_PACKAGES += \
